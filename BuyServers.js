9b72fff1b1dfa1fe26b0f0fb5f44a5b45b807ca6
Felix Schmäche <Felix.Schmaeche@evia.de>
2022-01-16 13:48:02 +0100

    init

--- BuyServers.js
+++ BuyServers.js
@@ -10,13 +10,13 @@ export async function main(ns) {
     ns.disableLog("getPurchasedServerCost")
     while (true) {
         ns.clearLog()
-        ns.print(ns.getPurchasedServerCost(ram) + "-" + ns.getServerMoneyAvailable("home").toFixed(0))
+        ns.print("Need " + ((ns.getPurchasedServerCost(ram) - ns.getServerMoneyAvailable("home").toFixed(0)) / 1000000).toFixed(2) + " M$ more!")
         if (ns.getPurchasedServerCost(ram) < ns.getServerMoneyAvailable("home").toFixed(0)) {
             let NewServerName = ServerNameStart + ServerNumber.toString().padStart(3, "0")
             ns.print(NewServerName);
             ns.purchaseServer(NewServerName, ram)
             ServerNumber = ServerNumber + 1
-            await ns.run("leetchOneServer.js", 1, "iron-gym")
+            await ns.run("LeetchOneServer.js", 1, "iron-gym")
         }
         await ns.sleep(1000)
     }
